5e183a21d47992f1c1637ed97ce8bf799961cfaa
Vince Carey <stvjc@channing.harvard.edu>
2017-05-05 14:45:12 -0400

    documented new dataset interface
    Former-commit-id: 8947277bee844e1928a4b8597442a25ec0abab9c

--- R/h5serv.R
+++ R/h5serv.R
@@ -1,23 +1,62 @@
-
+#' H5S_source identifies an HDF5 server and manages some metadata about contents
+#' 
+#' @name H5S_source
+#' @rdname H5S_source-class
+#' @slot serverURL character string with a URL
+#' @slot dsmeta DataFrame instance with metadata about content of server
+#' @exportClass H5S_source
 setClass("H5S_source", representation(
 # "host" content
    serverURL="character", dsmeta="DataFrame"))
 setMethod("show", "H5S_source", function(object) {
 cat("HDF5 server domain: ", object@serverURL, "\n")
-cat(" There are", nrow(grps <- groups(object)), "groups.\n")
+cat(" There are", nrow(object@dsmeta), "groups.\n")
 cat(" Use groups(), links(), ..., to probe and access metadata.\n")
 cat(" Use dsmeta() to get information on datasets within groups.\n")
+cat(" Use [[ [dsname] ]]  to get a reference suitable for [i, j] subsetting.\n")
 })
+
+#' name H5S_dataset
+#' rdname H5S_dataset-class
+#' @slot source instance of H5S_source instance
+#' @slot simpleName character string naming dataset 
+#' @slot shapes list including dimension information
+#' @slot hrefs DataFrame of hrefs as defined in the API
+#' @slot allatts list of all attributes
+#' @slot presel string prepared for select operation in GET
+#' @exportClass H5S_dataset
+setClass("H5S_dataset", representation(
+  source="H5S_source", simpleName="character",
+  shapes="list", hrefs="DataFrame", allatts="list", presel="character"))
+setMethod("show", "H5S_dataset", function(object) {
+ cat("H5S_dataset instance:\n")
+# aa = object@allatts
+# alld = sapply(aa, function(x) paste(x$shape$dims, collapse=" x "))
+# cr = sapply(aa, function(x) x$created)#, collapse=" x "))
+# ba = sapply(aa, function(x) x$type$base)
+ curdim = object@shapes$dims
+ print(data.frame(dsname=object@simpleName, intl.dim1=curdim[1], intl.dim2=curdim[2], 
+     created=object@allatts$created, type.base=object@allatts$type$base))
+#cat("Use [[", object@simplename, "]] to acquire reference amenable to [i,j] subsetting.\n")
+})
+
 
 fixtarget = function(x) sub(".*host=(.*).hdfgroup.org", "\\1", x)
 
 #' construct H5S_source
+#' @name H5S_source
+#' @rdname H5S_source-class
 #' @param serverURL a URL for a port for HDF5Server
 #' @param \dots not used
+#' @note The dsmeta slot holds a DataFrame with a column \code{dsnames}
+#' that is a list with ith element a character vector of all dsnames
+#' available for the ith group.  There is no effort at present to
+#' search all groups for candidate datasets.
 #' @examples
 #' bigec2 = H5S_source("http://54.174.163.77:5000")
 #' bigec2
-#' dsmeta(bigec2)[1:2,]
+#' dsmeta(bigec2)[1:2,] # two groups
+#' dsmeta(bigec2)[1,2][[1]] # all dataset candidates in group 1
 #' @export
 H5S_source = function(serverURL, ...) {
   tmp = new("H5S_source", serverURL=serverURL, dsmeta=DataFrame())
@@ -35,6 +74,12 @@ H5S_source = function(serverURL, ...) {
 dsmeta = function(src) {
   src@dsmeta
 }
+#' @rdname H5S_source-class
+#' @exportMethod [[
+setMethod("[[", c("H5S_source", "character", "ANY"), function(x, i, j) {
+  dataset(x, i)
+})
+
 
 
 setClass("H5S_linkset", representation(links="list", group="character",
@@ -93,8 +138,6 @@ targetIds = function(h5linkset, index) {
  sapply(h5linkset@links$links, "[[", "id")
  }
       
-setClass("H5S_dataset", representation(
-  target="character", linkset="H5S_linkset"))
 
 
 makeDatasetRef = function(linkset, index) {
@@ -105,6 +148,7 @@ makeDatasetRef = function(linkset, index) {
 }
 transl = function(targ)  fromJSON(readBin(GET(targ)$content, w="character"))
 
+# SHOULD BE DELETED
 setClass("H5S_datasets", representation(
   source="H5S_source", index="numeric", drop="ANY", simpleNames="character",
   shapes="list", hrefs="list", allatts="list"))
@@ -118,6 +162,7 @@ setMethod("show", "H5S_datasets", function(object) {
 cat("---\n")
 cat("Use [[ [simplename] ]] to acquire reference amenable to [i,j] subsetting.\n")
 })
+# PROBABLY NOT NEEDED
 setClass("RESTfulH5", representation(key="character",
  dataURL="character", dataset="H5S_datasets"))
 #' @exportMethod show
@@ -126,13 +171,15 @@ setMethod("show", "RESTfulH5", function(object) {
  cat(paste(object@key, "(", paste(dims, collapse=" x "), ")"), "\n")
 })
 
+#' @name H5S_dataset
+#' @rdname H5S_source-class
 #' @exportMethod [
-setMethod("[", c("RESTfulH5", "character", "character"), function(x, i, j, ..., drop=FALSE) {
+setMethod("[", c("H5S_dataset", "character", "character"), function(x, i, j, ..., drop=FALSE) {
 #
 # bracket selection passed directly to HDF5 server ... row-major
 #
- uu = x@dataURL
- dims = x@dataset@allatts[[ x@key ]]$shape$dims
+ uu = x@presel
+ dims = x@shapes$dims
  ind1lims = as.numeric(strsplit(i, ":")[[1]])
  if (ind1lims[1] < 0) stop("negative starting index not allowed in i")
  if (ind1lims[2] > dims[1]) stop("i exceeds boundary for first index")
@@ -146,55 +193,20 @@ setMethod("[", c("RESTfulH5", "character", "character"), function(x, i, j, ..., 
  else return(val)
 })
 
-#' @exportMethod [[
-setMethod("[[", c("H5S_datasets", "character"), function(x, i, j, ...) {
- sel = x@hrefs[[i]] # DataFrame
- sel = sel["self", 2]
- prep = sub("\\?host=", "/value?host=", sel)
- prep = paste0(prep, "&select=[%%SEL1%%,%%SEL2%%]")
- new("RESTfulH5", key=i, dataURL=prep, dataset=x)
-})
 
-#' obtain dataset metadata from a set of links
-#' @param linkset instance of H5S_linkset
-#' @param index numerical index of group to use
-#' @param drop numerical indices of hosts to be ignored
+#' @name dataset
+#' @rdname H5S_source-class
+#' @param h5s instance of H5S_source
+#' @param tag character string identifying a dataset
 #' @export
-datasetRefs = function(linkset, index, drop=5) {
- ho = hosts(linkset, index)
- if (!is.null(drop)) ho=ho[-drop]
- source = linkset@source
- uuids = sapply(1:length(ho), function(host) {
-  targ = paste0(source@serverURL, "/datasets?",  ho[host])
-#  print(targ)
-  fromJSON(readBin(GET(targ)$content, w="character"))$datasets
- })
- attrs = lapply(1:length(uuids), function(x) {
-  targ = paste0(source@serverURL, "/datasets/", uuids[x], "?", ho[x])
-  transl(targ)
-  })
- HRperDS = lapply(attrs, function(x) {
-    attnm = sapply(x$hrefs, "[[", 2)
-    attval = sapply(x$hrefs, "[[", 1)
-    ans = DataFrame(hrefName=attnm, hrefValue=attval)
-    rownames(ans) = attnm
-    ans
-    })
- selves = sapply(HRperDS, function(x) x["self", "hrefValue"])
- simpleNames = sapply(HRperDS, function(x) sub(".*host=(.*).hdfgroup.org", "\\1", x[2,2]))
- names(attrs)=simpleNames
- names(HRperDS) = simpleNames
-# list(simpleNames=simpleNames, attrs=attrs, selves=selves)
- new("H5S_datasets", source=linkset@source, index=index,
-       drop=drop, simpleNames=simpleNames, shapes=list(), hrefs=HRperDS, allatts=attrs)
-}
-
 dataset = function(h5s, tag) {
  dsns = dsmeta(h5s)[,"dsnames"]
  hits = sapply(dsns, function(x) length(grep(tag, x))>0)
  if (!any(hits)) stop("tag not found in dsmeta(h5s)")
  if (sum(hits)>1) warning("tag occurs in several groups, using first")
- grp = dsns[which(hits),,drop=FALSE]
+ #grp = dsns[which(hits),,drop=FALSE]
+ fulldsn = dsns[which(hits)][[1]] # unlist
+ fulldsn = fulldsn[ grep(tag, fulldsn) ]
  lin = links(h5s, which(hits))
  targs = targets(lin)
  targs = targs[grep(tag,targs)]
@@ -205,8 +217,17 @@ dataset = function(h5s, tag) {
       }
  targ = sub(".host", "datasets?host", targs)
  uuid = fromJSON(readBin(GET(targ)$content, w="character"))$datasets
- attrs = transl( sub("datasets", paste0("datasets/", uuid), targs ) )
- attrs
+ attrs = transl( sub("datasets", paste0("datasets/", uuid), targ ) )
+ hrnm = sapply(attrs$hrefs, "[[", 2)
+ hrval = sapply(attrs$hrefs, "[[", 1)
+ ans = DataFrame(hrefName=hrnm, hrefValue=hrval)
+ rownames(ans) = hrnm
+ self = ans["self", "hrefValue"]
+ prep = sub("\\?host=", "/value?host=", self)
+ prep = paste0(prep, "&select=[%%SEL1%%,%%SEL2%%]")
+ list(uuid=uuid, hrefs=ans, attrs=attrs)
+ new("H5S_dataset", source=h5s, simpleName=fulldsn,
+    shapes=attrs$shape, hrefs=ans, allatts=attrs, presel=prep)
 }
 
 
