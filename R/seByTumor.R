43b19b4e3977e974c72a00b3f60926a47e1821c3
Vince Carey <stvjc@channing.harvard.edu>
2017-08-03 19:04:47 -0400

    seByTumor defines BQSummarizedExperiment and there is an assay method
    Former-commit-id: f83927bd4450866da504f6099dbd7ad2673299df

--- R/seByTumor.R
+++ R/seByTumor.R
@@ -30,6 +30,11 @@ seByTumor_2016 = function(tumorCode = "LUAD", assayTblName="mRNA_UNC_HiSeq_RSEM"
  SummarizedExperiment(colData=as.data.frame(clin), rowData=DataFrame(assay %>% as.data.frame()))
 }
 
+#' @exportClass BQSummarizedExperiment
+setClass("BQSummarizedExperiment", contains="SummarizedExperiment",
+   representation(rowQref = "ANY", colQref = "ANY",
+   rowkey="character", colkey="character", assayvbl = "character"))
+
 #' given a BigQueryConnection to the 2017 GDC-oriented ISB TCGA bigtables, obtain a SummarizedExperiment 'shell' rowData and colData
 #' @param tumorCode one of the concise TCGA codes in a character string -- not checked, defaults to "LUAD", lung adenocarcinoma
 #' @param assayTblName the name of the assay whose annotation will be used as rowData
@@ -42,16 +47,18 @@ seByTumor_2016 = function(tumorCode = "LUAD", assayTblName="mRNA_UNC_HiSeq_RSEM"
 #' if (exists(".cgcBilling")) {
 #'  clinQ = cgcConn()
 #'  assayQ = cgcConn( dataset = "TCGA_hg38_data_v0" )
-#'  myexpShell = seByTumor_GDC( bqConnClinical=clinQ,
+#'  myexpShell = seByTumor( bqConnClinical=clinQ,
 #'        bqConnAssay=assayQ)
-#'  nrow(myexpShell) == 20531
-#'  ncol(myexpShell) == 522
+#'  print(nrow(myexpShell) == 60483)
+#'  print(ncol(myexpShell) == 522)
+#'  assay(myexpShell[11:15,1:4]) # some case_barcodes repeat
 #'  }
 #' @export
-seByTumor_GDC = function(tumorCode = "LUAD", 
+seByTumor = function(tumorCode = "LUAD", 
       assayTblName="RNAseq_Gene_Expression",
       rdColsToKeep = c("gene_name", "Ensembl_gene_id", "gene_type"),
-      bqConnClinical, bqConnAssay ){
+      bqConnClinical, bqConnAssay, rowkey="Ensembl_gene_id",
+      colkey = "case_barcode", assayvbl="HTSeq__Counts" ){
 #
 # assumes bqConn is a live instance of bigrquery BigQueryConnection
 #
@@ -59,9 +66,34 @@ seByTumor_GDC = function(tumorCode = "LUAD",
  if (bqConnAssay@dataset != "TCGA_hg38_data_v0") warning("bqConnAssay dataset name not 'TCGA_hg38data_v0', table/column-name expectations may fail")
  require(SummarizedExperiment)
  newn = paste0("TCGA-", tumorCode)
- clin = bqConnClinical %>% tbl("Clinical") %>% filter(project_short_name==newn)
+ clinRef = bqConnClinical %>% tbl("Clinical")
+ clin = clinRef %>% filter(project_short_name==newn)
  h = clin %>% select(case_barcode) %>% head() %>% as.data.frame()
  keeper = h[[1]][1]
- assay = bqConnAssay %>% tbl(assayTblName) %>% filter(case_barcode==keeper) %>% select(rdColsToKeep)
- SummarizedExperiment(colData=as.data.frame(clin), rowData=DataFrame(assay %>% as.data.frame()))
+ assayRef = bqConnAssay %>% tbl(assayTblName)
+ assay = assayRef %>% filter(case_barcode==keeper) %>% select(rdColsToKeep)
+ se = SummarizedExperiment(colData=as.data.frame(clin), rowData=DataFrame(assay %>% as.data.frame()))
+ new("BQSummarizedExperiment", se, rowQref = assayRef,
+     colQref = clinRef, rowkey=rowkey, colkey=colkey, assayvbl=assayvbl)
 }
+
+#' extract assay data
+#' @importFrom reshape2 dcast
+#' @note Very experimental approach to retrieving numerical data given
+#' a SummarizedExperiment 'shell'.  We need more checking of
+#' consistency between assay and clinical data before creating the
+#' shell.  We use dcast to transform query result to a matrix, and
+#' some 'individuals' may have multiple contributions ... we use
+#' \code{fun.aggregate = max} and will see warnings until this is cleared up.
+#' @exportMethod assay
+setMethod("assay", c("BQSummarizedExperiment", "missing"), 
+  function(x, i, ...) {
+  rd = rowData(x)[,x@rowkey]
+  cd = colData(x)[,x@colkey]
+  df = x@rowQref %>% select(one_of(c(x@rowkey, x@colkey, x@assayvbl))) %>% filter_(paste0(x@rowkey, " %in% rd & ", x@colkey, " %in% cd")) %>% # assumes colkey shared to assay table
+      as.data.frame()
+  res = dcast(df, as.formula(paste0(x@rowkey, "~", x@colkey)), fun.aggregate=max, value.var=x@assayvbl)
+  mat = data.matrix(res[,-1])
+  rownames(mat) = as.character(res[,1])
+  mat
+})
