c2cadbb9c26d50afb7f3f65c3dbf3c543a79b53c
Vince Carey <stvjc@channing.harvard.edu>
2018-04-13 01:26:31 -0400

    general use of DelayedArray for all constructed SummarizedExperiment instances in demo.R etc.  Some additional evaluations in the vignette
    Former-commit-id: 71c1cfcdd847c8a669c4e944663e337c36bc52d9 [formerly 32c776f7af9b7c3fe2d029cd97c4e378abc5c45f [formerly f17e0344eb71a3b4de6983e68d76a39be05c849b]] [formerly e98372e534bfea3b4b8caa07bf10ef60cec1d6da [formerly f8f25cad51b87cee3cbddf59268bb5d6d1f94a87] [formerly ef5aa476e7ce352ab6c46e0f688be98691386c79 [formerly 3111125b4173060672883638c2cbad808a6e6cfa]] [formerly e98372e534bfea3b4b8caa07bf10ef60cec1d6da [formerly f8f25cad51b87cee3cbddf59268bb5d6d1f94a87] [formerly ef5aa476e7ce352ab6c46e0f688be98691386c79 [formerly 3111125b4173060672883638c2cbad808a6e6cfa]] [formerly 32c776f7af9b7c3fe2d029cd97c4e378abc5c45f [formerly f17e0344eb71a3b4de6983e68d76a39be05c849b] [formerly 83628982c5ee24746fa1bbc2c0131dc6ea531e2f [formerly a0164c75d7e76ce18079ade9d135fb1cd9fed1d5]]]]]
    Former-commit-id: d3b157581e0f4df57f3b7c7eda0f4188f72d5c82 [formerly 83628982c5ee24746fa1bbc2c0131dc6ea531e2f]
    Former-commit-id: d8574d1fc5f6266d3ac7695cd0e3507ecea2e8aa
    Former-commit-id: d65f2c6e0ea1dd2ff24f5919c9b797d41cdbeba7 [formerly a0981d26d8c19fc8f267f4442a172a351e4eca7b] [formerly 6e11c77a416b8e5679dfe436022f7c3216846b36 [formerly 1390ce52228d0c43e9bc162fa667e35776972a8f]]
    Former-commit-id: a9a7598667d719a408420aabb9db7cd2d479af91 [formerly d89108ff0ac7e1ab595415b3b1fbd9f6fd3fe77e]
    Former-commit-id: 3eeef3c3f66ae12fb114cd41bdeb40aed4f757a2

---
+++ R/pancan_SE.R
@@ -0,0 +1,61 @@
+#' illustrate DelayedArray assay from BigQuery backend in SummarizedExperiment
+#' @importFrom rlang sym UQ
+#' @param bqcon a BigQueryConnection instance
+#' @param colDataTableName character(1) defaulting to "clinical_PANCAN_patient_with_followup"
+#' @param colDSubjectIdName character(1) defaulting to "bcr_patient_barcode"
+#' @param colDFilterField character(1) defaulting to "acronym"
+#' @param colDFilterValue character(1) defaulting to "BRCA"
+#' @param assayDataTableName character(1) defaulting to "pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_16_annot"
+#' @param assayFeatureName character(1) defaulting to "ID"
+#' @param subjectIDName character(1) defaulting to "ParticipantBarcode"
+#' @param tumorFieldName character(1) defaulting to "Study"
+#' @param tumorFieldValue character(1) defaulting to "BRCA"
+#' @param assayValueFieldName character(1) defaulting to "miRNAexpr"
+#' @note The parameters need different assignments for different tables.
+#' Field names are not standardized across tables.
+#' @return an instance of SummarizedExperiment
+#' @examples
+#' if (interactive() & requireNamespace("DBI") & 
+#'    requireNamespace("bigrquery")) {
+#'   pancan_BQ_loc = function (dataset="Annotated", 
+#'                 billing=Sys.getenv("CGC_BILLING")) 
+#'   {
+#'       con <- DBI::dbConnect(bigrquery::dbi_driver(), 
+#'          project = "pancancer-atlas",         
+#'          dataset = dataset, billing = billing)
+#'       con     
+#'   }
+#'   bqcon = try(pancan_BQ_loc()) # needs CGC_BILLING in environment
+#'   if (!inherits(bqcon, "try-error"))
+#'      pancan_SE(bqcon)
+#' }
+#' @export
+pancan_SE = function(bqcon,
+  colDataTableName = "clinical_PANCAN_patient_with_followup",
+  colDSubjectIdName = "bcr_patient_barcode",
+  colDFilterField = "acronym",
+  colDFilterValue = "BRCA",
+  assayDataTableName = "pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_16_annot",
+  assayFeatureName = "ID",
+  subjectIDName = "ParticipantBarcode",
+  tumorFieldName = "Study",
+  tumorFieldValue = "BRCA",
+  assayValueFieldName = "miRNAexpr") {
+ clinDF = bqcon %>% tbl(colDataTableName) %>%
+   filter(UQ(rlang::sym(colDFilterField)) == colDFilterValue) %>% as.data.frame(n=Inf)
+ rownames(clinDF) = clinDF[[colDSubjectIdName]]
+ clinDF = DataFrame(clinDF)
+ assaySrc = BQ3_Source(bqconn = bqcon,
+   tblnm = assayDataTableName,
+   rowkeyfield = assayFeatureName,
+   colkeyfield = subjectIDName,
+   filtervbl = tumorFieldName,
+   filterval = tumorFieldValue,
+   assayvbl = assayValueFieldName)
+ assayIni = BQ3_Array(assaySrc)
+ okids = intersect(rownames(clinDF), colnames(assayIni))
+ se = SummarizedExperiment(assayIni[, okids], colData = clinDF[okids, ])
+ names(assays(se)) = "assay"
+ se
+}
+
