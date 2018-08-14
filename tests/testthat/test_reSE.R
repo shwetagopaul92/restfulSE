bd3975ec730aa27037edddb6721c515da0103625
shwetagopaul <mail@shwetagopaul92.com>
2018-08-14 12:37:56 -0400

    change
    Former-commit-id: d7dec88252dd1d18b0f0152da54c3c0493d56b83 [formerly db91bd8357777dabcf035ec11b368136523494bf] [formerly dd92f3ada109291a13578024a9f782c418979338 [formerly baf3b27516be13e20d6f2c2f98550d8c69469e39]]
    Former-commit-id: 4f87721833ecf7aab8464ce4cd4f6bb988695df2 [formerly cc75abc3e73ba11f2502649567588e36fbf4c41f]
    Former-commit-id: 476b508629d58fce2b353c9208382c88d016e89d
    Former-commit-id: 79475d31b6e3bea4578f48b6d1c7bd3c5447fc6c [formerly 5a50b513e9f59e5f4c296ae89ab6b77df0cad356] [formerly ba7173f533fb80fe78cc70d1a0c5d750e6241791 [formerly 1248c92594a21d5589a8e4a30d1adb96f9540710]]
    Former-commit-id: daa642dff7f9697173f9d4922bf1c83f5a4aaeac [formerly 264e20165d5cf07b78e0ee3778cb1f9806448161]
    Former-commit-id: ac98ca4cdeb3d405da87bcff59bcfcdee1bc1d97
    Former-commit-id: dbd1a61320ec7b0ebebadbe5ae94f101fd8ae241 [formerly 602be97223db35e1321c19b24362bdea5ff97b16] [formerly 7fa57916266a21e80f3322cac937fa9fa0c53e2b [formerly 3e24bd67a7822697bfa4ac331300918eb971e27a]]
    Former-commit-id: 7036d85fa303f641e113e29dcd8305f50466b6a8 [formerly 791437f91a4f8eb087d43afb98ce883aec76fec1]
    Former-commit-id: b5e4b5fe7268751c36fec49c1318a93db1b2b62f
    Former-commit-id: 5d69fabe80d14810f6cd3b39f5fb82b269b6a855 [formerly 90734fcfe663366f6f1b47ecc63816756d60c749] [formerly e1ef2261bec1f6d4a86863b56a904f3e38e9267b [formerly 5ce64e3820e068a6fff7f8fb7e14b812481d4524]]
    Former-commit-id: 7d539f827ec2f28037c31f344a8bb5213e57a585 [formerly 6fbe0157ebd8bef1d430567945bb63a882057676]
    Former-commit-id: 0b49bf8c8f3fafa22ac72e077b7e86c3250188fa

--- tests/testthat/test_reSE.R
+++ tests/testthat/test_reSE.R
@@ -5,118 +5,157 @@ library(restfulSEData)
 context("connection")
 
 test_that("H5S_source completes", {
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- expect_true(is(bigec2, "H5S_source"))
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  expect_true(is(bigec2, "H5S_source"))
 })
 
 context("content wrapper structure") 
 
 test_that("H5S_source processes", {
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- expect_true(all(dim(rhdf5client::groups(bigec2))==c(10,2))) 
- expect_true(is(rhdf5client::links(bigec2,1), "H5S_linkset"))
- expect_true(is(dataset(bigec2, "tenx_100k"), "H5S_dataset"))
- expect_true(is(bigec2[["tenx_100k"]], "H5S_dataset"))
- expect_true(is(dsmeta(bigec2), "DataFrame"))
- expect_true(all(dim(dsmeta(bigec2))==c(10,3))) 
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  expect_true(all(dim(rhdf5client::groups(bigec2))==c(10,2))) 
+  expect_true(is(rhdf5client::links(bigec2,1), "H5S_linkset"))
+  expect_true(is(dataset(bigec2, "tenx_100k"), "H5S_dataset"))
+  expect_true(is(bigec2[["tenx_100k"]], "H5S_dataset"))
+  expect_true(is(dsmeta(bigec2), "DataFrame"))
+  expect_true(all(dim(dsmeta(bigec2))==c(10,3))) 
 })
 
 context("indexing infrastructure")
 
 test_that("sproc/isplit work", {
- expect_true(length(isplit(c(1,2,3,4,5,10,15,20,30:40)))==3)
- ii = isplit(c(1,2,3,4,5,10,15,20,30:40))
- ss = structure(c("0:5:1", "9:20:5", "29:40:1"), .Names = c("1", 
-"2", "3"))
- expect_true(identical(ss, unlist(sproc(ii))))
- ii = isplit(c(1:10, seq(50,25,-5), seq(80,100,2)))
- ss = structure(c("0:10:1", "50:24:-5", "79:100:2"), 
-                .Names = c("1", "2", "3"))
- expect_true(identical(ss, unlist(sproc(ii))))
+  expect_true(length(isplit(c(1,2,3,4,5,10,15,20,30:40)))==3)
+  ii = isplit(c(1,2,3,4,5,10,15,20,30:40))
+  ss = structure(c("0:5:1", "9:20:5", "29:40:1"), .Names = c("1", 
+                                                             "2", "3"))
+  expect_true(identical(ss, unlist(sproc(ii))))
+  ii = isplit(c(1:10, seq(50,25,-5), seq(80,100,2)))
+  ss = structure(c("0:10:1", "50:24:-5", "79:100:2"), 
+                 .Names = c("1", "2", "3"))
+  expect_true(identical(ss, unlist(sproc(ii))))
 })
 
 context("targets generation")
 
 test_that("targets method works", {
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- tt = targets(rhdf5client::links(bigec2, 1))
- expect_true(length(tt)>=9)  # july 5 2017
- expect_true(length(grep("host", tt))>=7) # added tenx_full dataset
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  tt = targets(rhdf5client::links(bigec2, 1))
+  expect_true(length(tt)>=9)  # july 5 2017
+  expect_true(length(grep("host", tt))>=7) # added tenx_full dataset
 })
 
 context("RESTful SE")
 
 test_that("RESTfulSummarizedExperiment infrastructure works against server", {
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- #data("st100k", package="restfulSEData")
- ehub = ExperimentHub::ExperimentHub()
- myfiles <- AnnotationHub::query(ehub , "restfulSEData")
- myfiles[["EH552"]] -> st100k
- n100k_served = bigec2[["tenx_100k_sorted"]]  
- rr = RESTfulSummarizedExperiment( st100k, n100k_served )
- expect_true(is(rr, "RangedSummarizedExperiment"))
- #data("banoSEMeta", package="restfulSEData")
- myfiles[["EH551"]] -> banoSEMeta
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- banoh5 = bigec2[["assays"]]
- rr = RESTfulSummarizedExperiment( banoSEMeta, banoh5 )
- expect_true(validObject(rr))
- expect_true(all(dim(rr)==rev(internalDim(banoh5))))
- rr2 = rr[5:12000,3:10]
- arr2 = assay(rr2)
- arr2s = structure(c(310.234826991, -574.26777552, 
-            -443.279313398, -8.31305800099999, 
-            -420.419219739, -41.648322087, 
-            160.146168073, -39.986112183), 
-         .Names = c("NA18501", "NA18502", "NA18516", "NA18517", 
-                     "NA18519", "NA18520", "NA18855", "NA18856"))
- csums = apply(arr2,2,sum)
- expect_true(max(abs(arr2s-csums)) < 1e-6)
-# sumstr = structure(c(3667, 1844, 4329, 2928, 7787, 10084, 2151, 3318, 
-#2851, 4158), .Names = c("AAACCTGAGATAGGAG-1", "AAACCTGAGCGGCTTC-1", 
-#"AAACCTGAGGAATCGC-1", "AAACCTGAGGACACCA-1", "AAACCTGAGGCCCGTT-1", 
-#"AAACCTGAGTCCGGTC-1", "AAACCTGCAACACGCC-1", "AAACCTGCACAGCGTC-1", 
-#"AAACCTGCAGCCACCA-1", "AAACCTGCAGGATTGG-1"))
-# expect_true(identical(apply(arr2,2,sum), sumstr))
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  #data("st100k", package="restfulSEData")
+  ehub = ExperimentHub::ExperimentHub()
+  myfiles <- AnnotationHub::query(ehub , "restfulSEData")
+  myfiles[["EH552"]] -> st100k
+  n100k_served = bigec2[["tenx_100k_sorted"]]  
+  rr = RESTfulSummarizedExperiment( st100k, n100k_served )
+  expect_true(is(rr, "RangedSummarizedExperiment"))
+  #data("banoSEMeta", package="restfulSEData")
+  myfiles[["EH551"]] -> banoSEMeta
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  banoh5 = bigec2[["assays"]]
+  rr = RESTfulSummarizedExperiment( banoSEMeta, banoh5 )
+  expect_true(validObject(rr))
+  expect_true(all(dim(rr)==rev(internalDim(banoh5))))
+  rr2 = rr[5:12000,3:10]
+  arr2 = assay(rr2)
+  arr2s = structure(c(310.234826991, -574.26777552, 
+                      -443.279313398, -8.31305800099999, 
+                      -420.419219739, -41.648322087, 
+                      160.146168073, -39.986112183), 
+                    .Names = c("NA18501", "NA18502", "NA18516", "NA18517", 
+                               "NA18519", "NA18520", "NA18855", "NA18856"))
+  csums = apply(arr2,2,sum)
+  expect_true(max(abs(arr2s-csums)) < 1e-6)
+  # sumstr = structure(c(3667, 1844, 4329, 2928, 7787, 10084, 2151, 3318, 
+  #2851, 4158), .Names = c("AAACCTGAGATAGGAG-1", "AAACCTGAGCGGCTTC-1", 
+  #"AAACCTGAGGAATCGC-1", "AAACCTGAGGACACCA-1", "AAACCTGAGGCCCGTT-1", 
+  #"AAACCTGAGTCCGGTC-1", "AAACCTGCAACACGCC-1", "AAACCTGCACAGCGTC-1", 
+  #"AAACCTGCAGCCACCA-1", "AAACCTGCAGGATTGG-1"))
+  # expect_true(identical(apply(arr2,2,sum), sumstr))
 })
- 
+
 test_that("complex indexing succeeds", {
-# library(yriMulti)
-# data(banovichSE)
-# bansel = assay(banovichSE[c(1,3,5,200000,300000),c(1,5,7)])
- bansel = structure(c(0.473396289, -0.222581829, -0.150630834, 1.754529575, 
--0.604892835, -0.903498324, -0.027544132, 0.596699897, 0.317356179, 
-0.393075519, 1.377916164, -1.473102055, -0.268610844, 0.63228941, 
--0.286316831), .Dim = c(5L, 3L), .Dimnames = list(c("cg00000029", 
-"cg00000236", "cg00000363", "cg16010467", "cg25249241"), c("NA18498", 
-"NA18516", "NA18519")))
- library(restfulSE)
- #data("banoSEMeta", package="restfulSEData") 
- ehub = ExperimentHub::ExperimentHub()
- myfiles <- AnnotationHub::query(ehub , "restfulSEData")
- myfiles[["EH551"]] -> banoSEMeta
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- banh = bigec2[["assays"]]
- rr = RESTfulSummarizedExperiment( banoSEMeta, banh )
- options(scipen = 999)
- rrsel = rr[c(1,3,5,200000,300000),c(1,5,7)]
- rrass = assay(rrsel)
- expect_true( max(abs(rrass-bansel))<1e-6 )
- rrsel0 = rr[c(1,3,5,200000,300000),c(1)]
- rrass0 = assay(rrsel0)
- expect_true(ncol(rrass0)==1)
+  # library(yriMulti)
+  # data(banovichSE)
+  # bansel = assay(banovichSE[c(1,3,5,200000,300000),c(1,5,7)])
+  bansel = structure(c(0.473396289, -0.222581829, -0.150630834, 1.754529575, 
+                       -0.604892835, -0.903498324, -0.027544132, 0.596699897, 0.317356179, 
+                       0.393075519, 1.377916164, -1.473102055, -0.268610844, 0.63228941, 
+                       -0.286316831), .Dim = c(5L, 3L), .Dimnames = list(c("cg00000029", 
+                                                                           "cg00000236", "cg00000363", "cg16010467", "cg25249241"), c("NA18498", 
+                                                                                                                                      "NA18516", "NA18519")))
+  library(restfulSE)
+  #data("banoSEMeta", package="restfulSEData") 
+  ehub = ExperimentHub::ExperimentHub()
+  myfiles <- AnnotationHub::query(ehub , "restfulSEData")
+  myfiles[["EH551"]] -> banoSEMeta
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  banh = bigec2[["assays"]]
+  rr = RESTfulSummarizedExperiment( banoSEMeta, banh )
+  options(scipen = 999)
+  rrsel = rr[c(1,3,5,200000,300000),c(1,5,7)]
+  rrass = assay(rrsel)
+  expect_true( max(abs(rrass-bansel))<1e-6 )
+  rrsel0 = rr[c(1,3,5,200000,300000),c(1)]
+  rrass0 = assay(rrsel0)
+  expect_true(ncol(rrass0)==1)
 })
 
 test_that("dim compatibility check is sensitive", {
- #data("banoSEMeta", package="restfulSEData") 
- ehub = ExperimentHub::ExperimentHub()
- myfiles <- AnnotationHub::query(ehub , "restfulSEData")
- myfiles[["EH551"]] -> banoSEMeta
- bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
- banoh5 = bigec2[["assays"]]
-<<<<<<< HEAD
- expect_error(rr==RESTfulSummarizedExperiment(banoSEMeta[-1,], banoh5) )
-=======
- expect_error( rr == RESTfulSummarizedExperiment(banoSEMeta[-1,], banoh5) )
->>>>>>> upstream/RELEASE_3_7
+  #data("banoSEMeta", package="restfulSEData") 
+  ehub = ExperimentHub::ExperimentHub()
+  myfiles <- AnnotationHub::query(ehub , "restfulSEData")
+  myfiles[["EH551"]] -> banoSEMeta
+  bigec2 = H5S_source("http://h5s.channingremotedata.org:5000")
+  banoh5 = bigec2[["assays"]]
+  expect_error( rr == RESTfulSummarizedExperiment(banoSEMeta[-1,], banoh5) )
+})
+
+
+doit = function() {library(restfulSE)
+  library(bigrquery)
+  library(DBI)
+  library(testthat)
+  library(reshape2)
+  NSAMP = 3
+  NFEAT = 2
+  
+  if (nchar(Sys.getenv("CGC_BILLING"))==0) skip("CGC_BILLING not set")
+  con = try(cgcConn("TCGA_hg38_data_v0"))
+  if (inherits(con, "try-error")) skip("can't establish CGC_BILLING")
+  ss = BQ3_Source(con, filterval="TCGA-LUAD")
+  samps = ss@allcolnames[sample(seq_len(length(ss@allcolnames)),NSAMP)]
+  feats = ss@allrownames[sample(seq_len(length(ss@allrownames)),NFEAT)]
+  myq = "SELECT ensembl_gene_id, case_barcode, HTSeq__Counts FROM `isb-cgc.TCGA_hg38_data_v0.RNAseq_Gene_Expression` where \n(case_barcode = '%s' or case_barcode = '%s' or case_barcode = '%s') and \n  project_short_name = 'TCGA-LUAD' and (ensembl_gene_id = '%s' or ensembl_gene_id = '%s')" 
+  myq = sprintf(myq, samps[1], samps[2], samps[3], feats[1], feats[2])
+  goodTable = DBI::dbGetQuery(con, myq)
+  expect_true(nrow(goodTable)==6) # add more tests for content of
+  # BQ3_Array execution
+  arr = BQ3_Array(ss)[c(feats[1], feats[2]),
+                      c(samps[1], samps[2], samps[3])]
+  expect_true(inherits(arr, "DelayedArray"))
+  mat = as.matrix(arr)
+  print(mat)
+  goodt = dcast(ensembl_gene_id~case_barcode, data=goodTable)
+  rownames(goodt) = goodt[,1]
+  print(goodt)
+  all.equal(data.matrix(goodt[rownames(mat), colnames(mat)]), mat)
+}
+
+#for(i in 1:20) {
+#  x = doit()
+#  cat(i)
+#  stopifnot(x==TRUE)
+#}
+
+context("bigquery accuracy")
+
+test_that("random query resolves directly and via BQ3_Array", {
+  expect_true(doit())
 })
