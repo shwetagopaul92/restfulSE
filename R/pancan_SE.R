e7dfcea3ca1d03394eb42e9e7a97f6d1317a3e97
Vince Carey <stvjc@channing.harvard.edu>
2018-08-21 13:31:57 -0400

    this version addresses authentication issues with more doc and tryCatch
    Former-commit-id: e92d8a11e5bd60a9413901eda7dec5727ee46d3a [formerly 55584ee0d2db97535b7116621c4505ea10125e7f] [formerly e1e85630424773c45b367d4b3b1d9d7a88b2fa9a [formerly 963ca3bbb05051d9a21146bc9f19340161f07352]]
    Former-commit-id: 439732abd28d714587870ad825da91b509398f71 [formerly a2f724196ad1f566536bd4bf680c98166aabd147]
    Former-commit-id: dc8a339273c3b778f460e1779cb9d89a15e87350

--- R/pancan_SE.R
+++ R/pancan_SE.R
@@ -2,37 +2,48 @@
 #' @importFrom rlang sym UQ
 #' @param bqcon a BigQueryConnection instance
 #' @param colDataTableName character(1) defaulting to "clinical_PANCAN_patient_with_followup"
+#' @param clinVars character() vector of variables to be retained from the table named by 'colDataTableName', defaults to vector returned by clinVars()
 #' @param colDSubjectIdName character(1) defaulting to "bcr_patient_barcode"
 #' @param colDFilterField character(1) defaulting to "acronym"
-#' @param colDFilterValue character(1) defaulting to "BRCA"
+#' @param colDFilterValue character(1) defaulting to "BRCA"; a vector may be
+#' used, in which case multiple tumor types will be 
+#' represented -- must agree with tumorFieldValue, as clinical
+#' and assay data are collected separately
 #' @param assayDataTableName character(1) defaulting to "pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_16_annot"
 #' @param assayFeatureName character(1) defaulting to "ID"
 #' @param assaySampleTypeCode character(1) defaulting to "TP"
 #' @param subjectIDName character(1) defaulting to "ParticipantBarcode"
 #' @param tumorFieldName character(1) defaulting to "Study"
-#' @param tumorFieldValue character(1) defaulting to "BRCA"
+#' @param tumorFieldValue character() defaulting to "BRCA"; a vector may be used in which case multiple tumor types will be represented -- must agree with colDFilterValue
 #' @param assayValueFieldName character(1) defaulting to "miRNAexpr"
 #' @note The parameters need different assignments for different tables.
-#' Field names are not standardized across tables.
+#' Field names are not standardized across tables as of August 2018.  AUTHENTICATION CONCERNS:
+#' You must have a valid BigQuery project identifier in the environment variable
+#' CGC_BILLING, or pass such as 'billing' when using DBI::dbConnect(bigquery::bigrquery(), ...).
+#' To get such a project identifier, you need to have a Google identity and you must
+#' have created a BigQuery project with that identity.  Notes at 
+#' \url{https://isb-cancer-genomics-cloud.readthedocs.io/en/latest/sections/progapi/bigqueryGUI/WalkthroughOfGoogleBigQuery.html} provide details.
+#' Another approach that can be used involves the Google Cloud SDK.  Assuming you have
+#' a Google identity and have installed a recent SDK instance, you can use (in the shell)
+#' \code{gcloud auth login} to pick the identity that has a project with id
+#' \code{PROJECT_ID}.  Use that project id as the billing code for bigrquery dbConnect,
+#' or set CGC_BILLING in the environment to evaluate to that project id.
 #' @return an instance of SummarizedExperiment
 #' @examples
 #' if (interactive() & requireNamespace("DBI") & 
 #'    requireNamespace("bigrquery")) {
-#'   pancan_BQ_loc = function (dataset="Annotated", 
-#'                 billing=Sys.getenv("CGC_BILLING")) 
-#'   {
-#'       con <- DBI::dbConnect(bigrquery::bigquery(), 
-#'          project = "pancancer-atlas",         
-#'          dataset = dataset, billing = billing)
-#'       con     
-#'   }
-#'   bqcon = try(pancan_BQ_loc()) # needs CGC_BILLING in environment
-#'   if (!inherits(bqcon, "try-error"))
-#'      pancan_SE(bqcon)
-#' }
+#'      billco = Sys.getenv("CGC_BILLING")
+#'      if (nchar(billco)==0) 
+#'           stop("need CGC_BILLING set to your BigQuery project ID, see note in ?pancan_SE")
+#'      bqcon = DBI::dbConnect(bigrquery::bigquery(), project = "pancancer-atlas", 
+#'            dataset = "Annotated", billing = billco)
+#'      brca_mirSE = pancan_SE(bqcon)
+#'      brca_mirSE
+#'      }
 #' @export
 pancan_SE = function(bqcon,
   colDataTableName = "clinical_PANCAN_patient_with_followup",
+  clinVars = basic_clinvars(),
   colDSubjectIdName = "bcr_patient_barcode",
   colDFilterField = "acronym",
   colDFilterValue = "BRCA",
@@ -43,8 +54,20 @@ pancan_SE = function(bqcon,
   tumorFieldName = "Study",
   tumorFieldValue = "BRCA",
   assayValueFieldName = "miRNAexpr") {
- clinDF = bqcon %>% tbl(colDataTableName) %>%
-   filter(UQ(rlang::sym(colDFilterField)) == colDFilterValue) %>% as.data.frame(n=Inf)
+ stopifnot(all.equal(colDFilterValue, tumorFieldValue))
+ msg = "Cannot probe requested clinical table.  This is probably an authentication
+issue.  Please read the note in ?pancan_SE to establish a valid .httr-oauth
+before retrying."
+ clinDF = tryCatch( bqcon %>% tbl(colDataTableName) %>% select(UQ(clinVars)) %>%
+   filter(UQ(rlang::sym(colDFilterField)) %in% colDFilterValue) %>% as.data.frame(n=Inf),
+     error = function(e) e )
+ if (inherits(clinDF, "simpleError")) stop(msg) # probably 404
+ if (inherits(clinDF, "bigrquery_notFound")) {
+   print(clinDF)
+   stop("Probably the value of environment variable CGC_BILLING 
+in the BigQueryConnection does not correspond to a valid BigQuery 
+project to which pancancer-atlas tables are linked.")
+   }
  rownames(clinDF) = clinDF[[colDSubjectIdName]]
  clinDF = DataFrame(clinDF)
  assaySrc = BQ3_Source(bqconn = bqcon,
@@ -62,3 +85,16 @@ pancan_SE = function(bqcon,
  se
 }
 
+#' provide a collection of basic clinical variables to limit size of clinical data return
+#' @return character(20) vector of variable names
+#' @note Use pancan_app() to determine other variables available
+#' @export
+basic_clinvars = function() {
+c("bcr_patient_uuid", "bcr_patient_barcode", "acronym", "gender", 
+"vital_status", "days_to_birth", "days_to_death", "days_to_last_followup", 
+"days_to_initial_pathologic_diagnosis", "age_at_initial_pathologic_diagnosis", 
+"icd_10", "tissue_retrospective_collection_indicator", "icd_o_3_histology", 
+"tissue_prospective_collection_indicator", "history_of_neoadjuvant_treatment", 
+"icd_o_3_site", "tumor_tissue_site", "new_tumor_event_after_initial_treatment", 
+"radiation_therapy", "race")
+}
