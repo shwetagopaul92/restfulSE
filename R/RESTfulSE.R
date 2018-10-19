b242910687369cdd3da916b8e9c79dcf15fed9b2
shwetagopaul <mail@shwetagopaul92.com>
2018-10-19 12:30:18 -0400

    removing references to ec2 hdf5 server except in tests, version bump 1.3.20
    Former-commit-id: 82a1b7eb1e9f161a04205b17a3bc2889b076b7d5 [formerly f8b18fc1b6b9ae763303bffc75b88d9f70c26dc2 [formerly 82a1b7eb1e9f161a04205b17a3bc2889b076b7d5 [formerly c4e56684363195e2d2e7256e51caef0010bbccf8]]] [formerly f9fb24b1643a9dba2dce8ae215a2c30b07b67b0e [formerly 3f1fe7ae488ae7bac535c9768634a098d0e85808] [formerly 722c655d472c28702e1218e87b5f599aaab0a001 [formerly 0003fc5626a5a0e56a2ced52d6e4b6ab142cf6e4]] [formerly 5e5d86b37768c6ed990e4f1bddbbc3ecefbad796 [formerly 11bf965116f4f3ac23f49dd9245826debd12fe92] [formerly 49e31091d8757cd5c4e273eb58486155d63cece6 [formerly 29ea0c07d6462f22e0024fe34dc739e617390e11]]] [formerly 4d7c04ee39d795dbd59388ec3445ef274035a3f4] [formerly 069afb552641e3726ae12df6ce00b682aba7af9d [formerly 255eebd03705a14f77d19f9e71c6c4efa2120956]] [formerly f9fb24b1643a9dba2dce8ae215a2c30b07b67b0e [formerly 3f1fe7ae488ae7bac535c9768634a098d0e85808] [formerly 722c655d472c28702e1218e87b5f599aaab0a001 [formerly 0003fc5626a5a0e56a2ced52d6e4b6ab142cf6e4]] [formerly 5e5d86b37768c6ed990e4f1bddbbc3ecefbad796 [formerly 11bf965116f4f3ac23f49dd9245826debd12fe92] [formerly 49e31091d8757cd5c4e273eb58486155d63cece6 [formerly 29ea0c07d6462f22e0024fe34dc739e617390e11]]] [formerly 4d7c04ee39d795dbd59388ec3445ef274035a3f4] [formerly 069afb552641e3726ae12df6ce00b682aba7af9d [formerly 255eebd03705a14f77d19f9e71c6c4efa2120956]] [formerly f8b18fc1b6b9ae763303bffc75b88d9f70c26dc2 [formerly 82a1b7eb1e9f161a04205b17a3bc2889b076b7d5 [formerly c4e56684363195e2d2e7256e51caef0010bbccf8]] [formerly 6707768239d596137e9f73f59e2331451dd991e6 [formerly f27fc1722be62b3bb980df76d11814e37ea0cbce] [formerly efaf0bf33862b986a75e522fac80e58642934d7c [formerly bc43937173ceb193533a86d8e3cf70f3ca25765b]] [formerly 6707768239d596137e9f73f59e2331451dd991e6 [formerly f27fc1722be62b3bb980df76d11814e37ea0cbce] [formerly efaf0bf33862b986a75e522fac80e58642934d7c [formerly bc43937173ceb193533a86d8e3cf70f3ca25765b]] [formerly 82a1b7eb1e9f161a04205b17a3bc2889b076b7d5 [formerly c4e56684363195e2d2e7256e51caef0010bbccf8] [formerly 255eebd03705a14f77d19f9e71c6c4efa2120956 [formerly d55c8162e2ca6e0fd2786974e81a2d4a4fbaaf86]]]]]]]]
    Former-commit-id: efaf0bf33862b986a75e522fac80e58642934d7c [formerly 6707768239d596137e9f73f59e2331451dd991e6 [formerly f27fc1722be62b3bb980df76d11814e37ea0cbce] [formerly efaf0bf33862b986a75e522fac80e58642934d7c [formerly bc43937173ceb193533a86d8e3cf70f3ca25765b]]]
    Former-commit-id: 6707768239d596137e9f73f59e2331451dd991e6 [formerly f27fc1722be62b3bb980df76d11814e37ea0cbce]
    Former-commit-id: 25ddb45d02a64a1a3c5db9d7fa080ab16bd141a6 [formerly 0bc579ba6992ade8510bd51616f0e9f2113df5b3] [formerly 5485884a24c55b21f8bf75e4ea31155432a35503 [formerly b3e3fee49910e1caf4fd204e03178cc22b93f34b [formerly 21db8bc7d1d0b6598ab7d5b7a4fbff6c0e1d8c0f] [formerly f7f98a513bf01f426373df735870ba0b6456540e [formerly 1c8f2cf0fb1105744e9461292c40307429b70f88]] [formerly 166ceb3b1e1117341783223a5ae63571353b93b5 [formerly 8f15e1aa9a8e6f6af8fac0a73926b04240c447aa] [formerly fffe21b123551f43ce8bc608a8a3ed51545c3b1f [formerly 0a063012e87e9cffbe0713c91da74cfbccfa22f0]]]]]
    Former-commit-id: d98add8e7078940a6052712bb9adb09badde6dac [formerly 7998535c7dee33b5ee7adb5ca631259ab3366728]
    Former-commit-id: 87b31c7492dddb6a3cd0c783a1889f15c5f4e463
    Former-commit-id: ca9ba0f86461099f0cb24f5a245e8a2be141a153 [formerly 107e95a6f3cf48c0ebdcdc5049382065b9bbadd0] [formerly 8121e61c3c8a23232ee6289be2ba585e73fd088f [formerly 73c004874b7b88197a2f72bac3c1199eb6dbc763]]
    Former-commit-id: da7c5cf7127c79fa86ab7021c394ba4524aac4a6 [formerly 768680c787b02ee4b83fa6c22fc780bd84d4e82f]
    Former-commit-id: 7313dc0cf2a634cdf37e256c0ed4600cb8a5c495

--- R/RESTfulSE.R
+++ R/RESTfulSE.R
@@ -15,8 +15,10 @@ setClass("RESTfulSummarizedExperiment",
 #' @param source instance of H5S_dataset
 #' @examples
 #' require("rhdf5client")
-#' bigec2 = H5S_source(serverURL="http://h5s.channingremotedata.org:5000")
-#' banoh5 = bigec2[["assays"]] # banovichSE
+#' hsds = H5S_source(serverURL=URL_hsds())
+#' hsdsCon = setPath(hsds,"/home/reshg/bano_meQTLex.h5")
+#' fetchDatasets(hsdsCon)
+#' banoh5 = H5S_dataset2(hsdsCon,"d-435d7ad4-9f13-11e8-92c2-0242ac120021")
 #' ehub = ExperimentHub::ExperimentHub()
 #' myfiles <- AnnotationHub::query(ehub , "restfulSEData")
 #' myfiles[["EH551"]] -> banoSEMeta
